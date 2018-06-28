5953c921c91c1da405469ca33c56d28348e4a6ea
xNikos <nikos.sinx@gmail.com>
2018-06-28 02:23:10 +0200

    Added Popups

--- app/src/main/java/com/rinworks/nikos/fuelfullpaliwoikoszty/MainActivity.java
+++ app/src/main/java/com/rinworks/nikos/fuelfullpaliwoikoszty/MainActivity.java
@@ -20,11 +20,14 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -32,18 +35,39 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.github.yavski.fabspeeddial.FabSpeedDial;
 import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     //private DrawerLayout drawer;
+    SwitchCompat switchCompat;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //inicjalizacja apki
+
+        switchCompat = findViewById(R.id.switcher);
+        NavigationView navigationView = findViewById(R.id.NavigationView);
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.findItem(R.id.nav_theme);
+        View actionToogleView = MenuItemCompat.getActionView(item);
+
+        switchCompat = actionToogleView.findViewById(R.id.switcher);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "Ciemny motyw: "+isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
 //        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
@@ -54,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
 //        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
 //        drawer.addDrawerListener(toggle);
 //        toggle.syncState();
+
+
 
 
         final FabSpeedDial mFab = findViewById(R.id.extendedFab); //extended fab init
@@ -89,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onMenuItemSelected(MenuItem menuItem) {
 
                 switch (menuItem.getItemId()) {
+                    //Tankowanie
                     case R.id.fab_tankowanie:
                         //Dialog create
                         AlertDialog.Builder popupBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -98,8 +125,14 @@ public class MainActivity extends AppCompatActivity {
                         TextView cenaL = mView.findViewById(R.id.CenaL);
                         TextView przejechano = mView.findViewById(R.id.PrzejechanoT);
                         final EditText zatankowanoV = mView.findViewById(R.id.ZatankowanioVALUE);
+                        zatankowanoV.setFilters(new InputFilter[]{new DecimalDigitsInputFilter
+                                (2, 2)});
                         final EditText CenaLV = mView.findViewById(R.id.CenaLValue);
+                        CenaLV.setFilters(new InputFilter[]{new DecimalDigitsInputFilter
+                                (1, 2)});
                         final EditText przejechanoV = mView.findViewById(R.id.PrzejechanoTValue);
+                        przejechanoV.setFilters(new InputFilter[]{new DecimalDigitsInputFilter
+                                (3, 2)});
                         Button okbtn = mView.findViewById(R.id.tankowanie_ok_btn);
                         Button cancelbtn = mView.findViewById(R.id.tankowanie_cancel_btn);
 
@@ -141,6 +174,115 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                         break;
+                    //Przypomnienie
+                    case R.id.fab_przypomnienie:
+                        //Dialog create
+                        AlertDialog.Builder popupBuilderNotification = new AlertDialog.Builder
+                                (MainActivity.this);
+                        View mViewNotification = getLayoutInflater().inflate(R.layout.przypomnienie_popup,
+                                null);
+                        TextView przypomnienie_title = mViewNotification.findViewById(R.id.przypomnienie_popup_title);
+                        TextView tytul = mViewNotification.findViewById(R.id.Tytuł);
+                        TextView tresc = mViewNotification.findViewById(R.id.Tresc);
+                        TextView kiedy = mViewNotification.findViewById(R.id.Kiedy);
+                        final EditText tytulV = mViewNotification.findViewById(R.id.TytulValue);
+                        final EditText trescV = mViewNotification.findViewById(R.id.TrescValue);
+                        final EditText kiedyV = mViewNotification.findViewById(R.id.KiedyV);
+                        okbtn = mViewNotification.findViewById(R.id.tankowanie_ok_btn);
+                        cancelbtn = mViewNotification.findViewById(R.id.tankowanie_cancel_btn);
+
+                        popupBuilderNotification.setView(mViewNotification);
+                        final AlertDialog dialogNotification = popupBuilderNotification.create();
+                        dialogNotification.show();
+
+                        okbtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (!trescV.getText().toString().isEmpty() && !tytulV.getText()
+                                        .toString().isEmpty() && !kiedyV.getText().toString().isEmpty()) {
+                                    //todo:Dodaj do bazy
+                                    dialogNotification.dismiss();
+                                    Snackbar mSnack = Snackbar.make(findViewById(R.id.card_recycler),
+                                            "Dodano do bazy! :)", Snackbar
+                                                    .LENGTH_LONG);
+                                    mSnack.show();
+
+                                } else {
+                                    Toast mToast = Toast.makeText(MainActivity.this, "Proszę wypełnij " +
+                                            "wszystkie pola!", Toast.LENGTH_SHORT);
+                                    mToast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL,
+                                            0, 0);
+                                    mToast.show();
+
+                                }
+                            }
+                        });
+
+                        cancelbtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialogNotification.dismiss();
+                                Snackbar mSnack = Snackbar.make(findViewById(R.id.card_recycler),
+                                        "Anulowano!", Snackbar.LENGTH_LONG);
+                                mSnack.show();
+                            }
+                        });
+                        break;
+                    //Naprawa
+                    case R.id.fab_naprawa:
+                        //Dialog create
+                        AlertDialog.Builder popupBuilderRepair = new AlertDialog.Builder
+                                (MainActivity.this);
+                        View mViewRepair = getLayoutInflater().inflate(R.layout.naprawa_popup,
+                                null);
+                        TextView naprawa_title = mViewRepair.findViewById(R.id.naprawa_popup_title);
+                        TextView Zaplacono = mViewRepair.findViewById(R.id.Zaplacono);
+                        TextView Naprawiono = mViewRepair.findViewById(R.id.Naprawiono);
+                        final EditText zaplaconoV = mViewRepair.findViewById(R.id.ZaplaconoValue);
+                        zaplaconoV.setFilters(new InputFilter[]{new DecimalDigitsInputFilter
+                                (4, 2)});
+                        final EditText naprawionoV = mViewRepair.findViewById(R.id.naprawionoVALUE);
+                        okbtn = mViewRepair.findViewById(R.id.tankowanie_ok_btn);
+                        cancelbtn = mViewRepair.findViewById(R.id.tankowanie_cancel_btn);
+
+                        popupBuilderRepair.setView(mViewRepair);
+                        final AlertDialog dialogRepair = popupBuilderRepair.create();
+                        dialogRepair.show();
+
+                        okbtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (!zaplaconoV.getText().toString().isEmpty() && !naprawionoV.getText()
+                                        .toString
+                                                ().isEmpty()) {
+                                    //todo:Dodaj do bazy
+                                    dialogRepair.dismiss();
+                                    Snackbar mSnack = Snackbar.make(findViewById(R.id.card_recycler),
+                                            "Dodano do bazy! :)", Snackbar
+                                                    .LENGTH_LONG);
+                                    mSnack.show();
+
+                                } else {
+                                    Toast mToast = Toast.makeText(MainActivity.this, "Proszę wypełnij " +
+                                            "wszystkie pola!", Toast.LENGTH_SHORT);
+                                    mToast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL,
+                                            0, 0);
+                                    mToast.show();
+
+                                }
+                            }
+                        });
+
+                        cancelbtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialogRepair.dismiss();
+                                Snackbar mSnack = Snackbar.make(findViewById(R.id.card_recycler),
+                                        "Anulowano!", Snackbar.LENGTH_LONG);
+                                mSnack.show();
+                            }
+                        });
+                        break;
                     //Default
                     default:
                         Toast.makeText(MainActivity.this, "Coś", Toast.LENGTH_SHORT).show();
@@ -154,3 +296,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
